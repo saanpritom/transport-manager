e715e6d3744f2a916dadc47edbd424a1e7ece3d1
Pritom Borogoria <pritom.saan@gmail.com>
2020-07-03 23:52:01 +0600

    AgencyModel installed and some dummy data are pushed

--- drf_transport_manager/models.py
+++ drf_transport_manager/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .configs import get_media_directory
 
 
 # Create your models here.
 
-""" VehicleType Model holds the data of the types of vehicle. Like Bus, Truck, Mini Truck etc.
+""" VehicleTypeModel holds the data of the types of vehicle. Like Bus, Truck, Mini Truck etc.
     Upon migrating this class seeding a list of default vehicle type values. These values are reside in
     on the vehicle_types.json file in the fixtures directory inside this app"""
 
 
-class VehicleType(models.Model):
+class VehicleTypeModel(models.Model):
     type_name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Type Name')
     is_active = models.BooleanField(default=True, verbose_name='Is Active')
-    created_by = models.ForeignKey(User, related_name='vehicle_types', on_delete=models.CASCADE, null=False,
+    created_by = models.ForeignKey(get_user_model(), related_name='vehicle_types', on_delete=models.CASCADE, null=False,
                                    verbose_name='Created by', default=1)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
@@ -27,7 +27,7 @@ class VehicleType(models.Model):
         ordering = ['type_name']
 
 
-""" VehicleManufacturer holds the data about the details of a vehicle manufacturer. Like a Manufacturer company
+""" VehicleManufacturerModel holds the data about the details of a vehicle manufacturer. Like a Manufacturer company
     name, country of origin, website, email, contact number and address. Some manufacturer data are initially
     given. You will find those initial on the vehicle_manufacturers.json file in the fixtures directory inside
     this app. In order to upload images of the manufactures logo please set MEDIA_ROOT on your settings.py file
@@ -35,7 +35,7 @@ class VehicleType(models.Model):
     this package is in the drf_transport_manager_images folder."""
 
 
-class VehicleManufacturer(models.Model):
+class VehicleManufacturerModel(models.Model):
     manufacturer_name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Manufacturer Name')
     country_of_origin = models.CharField(max_length=50, null=True, blank=True, verbose_name='Country of Origin')
     website_url = models.URLField(max_length=200, null=True, blank=True, verbose_name='Website')
@@ -45,7 +45,7 @@ class VehicleManufacturer(models.Model):
     logo = models.ImageField(upload_to=get_media_directory('drf_transport_manager_images/manufacturer_logo/'),
                              null=True, blank=True, verbose_name='Manufacturer Logo')
     is_active = models.BooleanField(default=True, verbose_name='Is Active')
-    created_by = models.ForeignKey(User, related_name='vehicle_manufacturers', on_delete=models.CASCADE, null=False,
+    created_by = models.ForeignKey(get_user_model(), related_name='vehicle_manufacturers', on_delete=models.CASCADE, null=False,
                                    verbose_name='Created by', default=1)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
@@ -57,3 +57,33 @@ class VehicleManufacturer(models.Model):
         verbose_name = 'Vehicle Manufacturer'
         verbose_name_plural = 'Vehicle Manufacturers'
         ordering = ['manufacturer_name']
+
+
+"""TransportAgencyModel holds the data belongs to a transport company. It is not necessary that every vehichle
+   belongs to an agency. In that case the vehicle will be a member of a N/A agency. The N/A instance ID will always
+   be 1. Here an agency can be a fully operational transport company or can be any individual person.
+   Some default data are on the transport_agencies.json file at the fixtures directory in this app"""
+
+
+class TransportAgencyModel(models.Model):
+    agency_name = models.CharField(max_length=80, null=False, blank=False, default='N/A',
+                                   verbose_name='Manufacturer Name')
+    contact_person = models.CharField(max_length=80, null=False, blank=False, default='N/A',
+                                      verbose_name='Contact Person Name')
+    website_url = models.URLField(max_length=200, null=True, blank=True, verbose_name='Website')
+    email = models.EmailField(max_length=254, null=True, blank=True, verbose_name='Email')
+    contact_number = models.CharField(max_length=50, null=True, blank=True, verbose_name='Contact Number')
+    address = models.TextField(null=True, blank=True, verbose_name='Address')
+    is_active = models.BooleanField(default=True, verbose_name='Is Active')
+    created_by = models.ForeignKey(get_user_model(), related_name='transport_agency', on_delete=models.CASCADE, null=False,
+                                   verbose_name='Created by', default=1)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
+    def __str__(self):
+        return self.agency_name
+
+    class Meta:
+        verbose_name = 'Transport Agency'
+        verbose_name_plural = 'Transport Agencies'
+        ordering = ['agency_name']
--- drf_transport_manager/tests.py
+++ drf_transport_manager/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from .configs import get_media_directory
+from .models import TransportAgencyModel
 
 
 # Create your tests here.
@@ -15,3 +17,14 @@ class ConfigModulesTestCase(TestCase):
         if hasattr(settings, 'MEDIA_ROOT'):
             self.assertEqual(get_media_directory('images/hello'), str(settings.MEDIA_URL[1:]) + 'images/hello')
         self.assertEqual(get_media_directory('images/hello'), 'media/images/hello')
+
+
+class ModelInitialDataTestCase(TestCase):
+
+    def setUp(self):
+        get_user_model().objects.create_user('test_admin', 'test_admin@example.com')
+        TransportAgencyModel.objects.create()
+
+    # Testing models dependent initial data
+    def test_transport_agency_model_initial_data(self):
+        self.assertEqual(TransportAgencyModel.objects.get(id=1).agency_name, 'N/A')
